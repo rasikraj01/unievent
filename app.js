5b481dc8d3ab9b73153b0aeeb234ef118dfe0e09
Rasik Raj <rasikraj01@gmail.com>
2018-07-04 01:48:11 -0700

    routes added

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const router = require('./routes/events_routes');
+// mongoose.connect('mongodb://localhost/ninjago');
+// mongoose.Promise = global.Promise;
 
-const app = express();
-
-app.use('/api', router);
+const routes = require('./routes/events_routes');
 
 const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.listen(PORT, () => {
-   console.log(`Server running at PORT : ${PORT}`);
-});
+// //serving static files
+// app.use(express.static('public'));
+
+//body-parser extract the entire body portion of an incoming request stream and exposes it on req.body
+app.use(bodyParser.json());
+
+//routes middleware
+app.use('/api', routes);
+
+// //error middleware
+// app.use(function(err, req, res, next) {
+//    res.status(422).send(err.message);
+// })
+
+app.listen(PORT , () => {
+   console.log(`Serving running at : ${PORT}`);
+})
--- routes/events_routes.js
+++ routes/events_routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('aa/', (req, res) => {
-   res.send('rasik');
-})
 
+router.get('/event', (req, res) => {
+    res.send('rasik')
+});
+
+router.post('/event',(req, res) => {
+    res.send('rasik_post')
+});
+
+router.put('event/:id', (req, res) => {
+    res.send(req.params.id);
+});
+router.delete('event/:id', (req, res){
+    res.send(req.params.id);
+});
 module.exports = router;
