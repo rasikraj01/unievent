c334444928b6b095d7d5dbe814e28191e82ddc09
Rasik <rasikraj01@gmail.com>
2018-07-10 00:19:52 +0530

    heroku update

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const KEY = require('./config/key');
+//const KEY = require('./config/key');
 
 // mongoose.connect('mongodb://localhost:27017/univent', { useNewUrlParser: true });
-mongoose.connect(`mongodb://${KEY.mLab_id}:${KEY.mLab_password}.mlab.com:31941/univent`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://univent_rasik:rasik123.mlab.com:31941/univent`, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 const routes = require('./routes/events_routes');
