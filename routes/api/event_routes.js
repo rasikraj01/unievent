06b7a7f54be9738dbf0ea623ee4c43c680329f4e
Rasik <rasikraj01@gmail.com>
2018-07-21 01:10:20 +0530

    updated route

--- routes/api/event_routes.js
+++ routes/api/event_routes.js
@@ -57,7 +57,35 @@ router.post('/', passport.authenticate('jwt', {session : true}) /*add authorizat
 
 
 router.put('/:id', passport.authenticate('jwt', {session : true}) /*add authorization*/,(req, res) => {
-
+   if(req.params.id){
+         // add record not found condition ps invalid id
+         Event.findById({_id : req.params.id}).then((result) => {
+            if(result.user == req.user.id){
+               const updatedEvent = {
+                  user: req.user.id,
+                  event_name : req.body.event_name,
+                  host_college : req.body.host_college,
+                  venue : req.body.venue,
+                  description : req.body.description,
+                  society : req.body.society,
+                  form_link : req.body.form_link,
+                  cover_link : req.body.cover_link,
+                  number_of_participants : req.body.number_of_participants,
+                  date : req.body.date,
+                  prizes_worth : req.body.prizes_worth,
+               }
+                Event.findByIdAndUpdate({_id : req.params.id}, updatedEvent).then((result) => {
+                   res.json(result)
+                }).catch((err) => {console.log(err);})
+            }
+            else {
+               res.json({message: 'You are not authorized to Update this Event'})
+            }
+         }).catch((err) => {console.log(err);})
+   }
+   else {
+      res.json({message: 'invalid update request'})
+   }
 })
 
 
@@ -76,7 +104,7 @@ router.delete('/:id', passport.authenticate('jwt', {session : true}) /*add autho
          }).catch((err) => {console.log(err);})
    }
    else {
-      res.json({message: 'invalid deleted request'})
+      res.json({message: 'invalid delete request'})
    }
 })
 
