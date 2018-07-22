cd0af605839a35e04c5ad4ebc5ef903c0fcd29c5
Rasik <rasikraj01@gmail.com>
2018-07-22 14:37:24 +0530

    heroku update

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const keys = require('./keys.js')
 
 module.exports = () => {
-   mongoose.connect(keys.db.mongodbURI, { useNewUrlParser: true }).then(() => console.log('Database Connected'));
+   mongoose.connect( process.env.monogoDBURI || keys.db.mongodbURI, { useNewUrlParser: true }).then(() => console.log('Database Connected'));
    mongoose.Promise = global.Promise;
 }
