df49476a6c47e47ef823c04b6891f17adb1e8282
Rasik <rasikraj01@gmail.com>
2018-08-01 19:58:10 +0530

    heroku update

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ require('./config/passport-setup')(passport)
 
 TODO:
 3. Keys protection -- heroku enviroment variables
-5. Data Validations
 9. Edge Cases review
 22. token expiration time :  https://stackoverflow.com/questions/27978868/destroy-cookie-nodejs
 
